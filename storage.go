27b5b4d538efd7bd91b737ce87381aa6e1c1c4c3
Javier Guajardo <javier.guajardoj@gmail.com>
2022-11-14 06:17:56 +0200

    Remove interval and store counters

--- storage.go
+++ storage.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"time"
 )
 
 var path = "/tmp"
@@ -27,19 +26,23 @@ func ImportData(customPath ...string) bool {
 	}
 	listsContent, errLists := ioutil.ReadFile(fmt.Sprintf("%s/lists.json", path))
 	keysContent, errKeys := ioutil.ReadFile(fmt.Sprintf("%s/keys.json", path))
+	countersContent, errCounters := ioutil.ReadFile(fmt.Sprintf("%s/counters.json", path))
 
-	if errLists != nil || errKeys != nil {
+	if errLists != nil || errKeys != nil || errCounters != nil{
 		return false
 	}
 
 	json_lists := make(map[string][]string)
 	json_keys := make(map[string]string)
+	json_counters := make(map[string]int64) 
 
 	json.Unmarshal(listsContent, &json_lists)
 	json.Unmarshal(keysContent, &json_keys)
+	json.Unmarshal(countersContent, &json_counters)
 
 	lists = json_lists
 	keys = json_keys
+	counters = json_counters
 
 	return true
 }
@@ -51,8 +54,7 @@ func SaveData(customPath ...string) { // execute as goroutine
 		}
 	}
 	fmt.Printf("Store data at folder '%s'\n", path)
-	for range time.Tick(10 * time.Second) {
-		SaveFile(fmt.Sprintf("%s/lists.json", path), lists)
-		SaveFile(fmt.Sprintf("%s/keys.json", path), keys)
-	}
+	SaveFile(fmt.Sprintf("%s/lists.json", path), lists)
+	SaveFile(fmt.Sprintf("%s/keys.json", path), keys)
+	SaveFile(fmt.Sprintf("%s/counters.json", path), counters)
 }
